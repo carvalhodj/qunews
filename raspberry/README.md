7947137eca94714e4dbee058239837bb8a421db3
Daniel de Carvalho <danieljosecarvalho@gmail.com>
2017-07-26 11:13:10 -0300

    Refatoração do código do coletor

--- raspberry/README.md
+++ raspberry/README.md
@@ -4,8 +4,11 @@
    dispositivo Raspberry Pi;
    
  - Lembrar de executar os comandos com '**sudo**';
+
+ - Executar o arquivo **mac_amqp.py** com o endereço IP do servidor RabbitMQ como parâmetro:
+    - ``sudo python mac_amqp 1.2.3.4 ``
  
  - As bibliotecas python estão descritas no arquivo
    '**rasp_requirements.txt**', a serem instalados com o '**pip**' pelo comando:
 
-    - sudo pip install -r rasp_requirements.txt
+    - ``sudo pip install -r rasp_requirements.txt``
--- raspberry/mac_amqp.py
+++ raspberry/mac_amqp.py
@@ -1,15 +1,21 @@
 import pcap, dpkt, binascii
 import pika
+import sys
 
 #connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.0.108'))
 
+end_ip = sys.argv[1:]
+
+if not end_ip:
+    print >> sys.stderr, "Fomato: %s [SERVER IP ADDRESS]" % (sys.argv[0])
+
 credentials = pika.PlainCredentials('qunews', 'qunews')
-connection = pika.BlockingConnection(pika.ConnectionParameters('192.168.0.108', 5672, 'qunews_host', credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(sys.argv[1], 5672, 'qunews_host', credentials))
 channel = connection.channel()
 
 channel.exchange_declare(exchange='qunews.data', type='topic', durable=True)
 
-routing_key = 'qunews'
+routing_key = 'qunews.users.mac'
 
 def enviaAMQP(rk, msg):
     return channel.basic_publish(exchange='qunews.data', routing_key=rk, body=msg)
@@ -23,6 +29,7 @@ for ts, pkt in pcap.pcap(name='wlan0'):
     if wifi.type == 0 and wifi.subtype == 4:
         src = binascii.hexlify(wifi.mgmt.src)
         ssid = wifi.ies[0].info
-        enviaAMQP(routing_key, src)   
-        print(ts, src, ssid)
-        print("\n")
+        mensagem = str(ts) + ',' + src + ',' + ssid
+        enviaAMQP(routing_key, mensagem)   
+#        print(ts, src, ssid)
+#        print("\n")
