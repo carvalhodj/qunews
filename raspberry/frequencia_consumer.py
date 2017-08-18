612ff72e89707ec6a32d3cbe6f7a9bddbf9f4850
Daniel de Carvalho <danieljosecarvalho@gmail.com>
2017-08-18 09:06:09 -0300

    Rasp calculando a frequência relativa vindo do Spark

--- raspberry/frequencia_consumer.py
+++ raspberry/frequencia_consumer.py
@@ -2,7 +2,7 @@ import pika
 
 credentials = pika.PlainCredentials('qunews', 'qunews')
 connection = pika.BlockingConnection(pika.ConnectionParameters(
-               '172.16.188.133', 5672, 'qunews_host', credentials))
+               '10.0.0.17', 5672, 'qunews_host', credentials))
 
 channel = connection.channel()
 
@@ -20,9 +20,31 @@ channel.queue_bind(exchange='qunews.data',
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
+def calcula_total_macs(macs):
+    total = 0
+    
+    for tipo in macs:
+        total += int(tipo[1])
+    
+    return total
+
+
+def calcula_frequencias(lista_macs):
+    total_macs = calcula_total_macs(lista_macs)
+    lista_frequencias = []
+
+    for tipo in lista_macs:
+        frequencia_relativa = float(tipo[1])/total_macs
+        frequencia_relativa_rounded = round(frequencia_relativa, 2)
+        lista_frequencias.append((tipo[0], frequencia_relativa_rounded))
+
+    return lista_frequencias
+
+
 def callback(ch, method, properties, body):
     texto = body.decode()
-    print(texto)
+    lista = calcula_frequencias(eval(texto))
+    print(lista)
 
 channel.basic_consume(callback,
                       queue=queue_name,
